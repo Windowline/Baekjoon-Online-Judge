3e90749b7810524157415f5bcfcf0406fc37abb5
Changsun Park <windowline1206@naver.com>
2016-06-15 13:50:42 +0900

    Create _2576.cpp

---
+++ _2576.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int sum = 0;
+    int min = 101;
+    for (int i = 0; i < 7; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        if (n % 2 == 1)
+        {
+            sum += n;
+            if (min > n)
+                min = n;
+        }
+    }
+    if (sum == 0)
+        printf("-1\n");
+    else
+    {
+        printf("%d\n", sum);
+        printf("%d\n", min);
+    }
+ 
+}
