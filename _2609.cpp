d270e834fe03fa259cec94fce04896aecce30ae4
winsun1206 <>
2021-07-12 11:33:51 +0900

    indent test

--- _2609.cpp
+++ _2609.cpp
@@ -1,17 +1,15 @@
 #include<stdio.h>
-int gcd(int m, int n)
-{
+int gcd(int m, int n) {
 	if (m%n == 0)
 		return n;
 	else
 		gcd(n, m%n);
 }
-int lcm(int m, int n)
-{
+
+int lcm(int m, int n) {
 	int t1 = m;
 	int t2 = n;
-	while (true)
-	{
+	while (true) {
 		if (t1 > t2)
 			t2 += n;
 		else if (t1 < t2)
@@ -22,21 +20,19 @@ int lcm(int m, int n)
 
 	return t1;
 }
-int main()
-{
+
+int main() {
 	int a, b, m, n;
 	scanf("%d %d", &a, &b);
 
-	if (a >= b)
-	{
+	if (a >= b) {
 		m = a;
 		n = b;
-	}
-	else
-	{
+	} else {
 		n = a;
 		m = b;
 	}
+	
 	printf("%d\n", gcd(m, n));
 	printf("%d\n", lcm(m, n));
 
